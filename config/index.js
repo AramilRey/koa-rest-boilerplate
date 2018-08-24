a131c4b3be32fad4bd27c2f2bd902190d37f1f27
Aramil Rey <aramilrey94@gmail.com>
2018-08-24 12:54:15 -0300

    Update index.js

--- config/index.js
+++ config/index.js
@@ -5,8 +5,6 @@ const convict = require('convict');
 const schema = require('./schema.js');
 
 const config = convict(schema);
-
-// Perform validation
 config.validate({ allowed: 'strict' });
 
 module.exports = config;
